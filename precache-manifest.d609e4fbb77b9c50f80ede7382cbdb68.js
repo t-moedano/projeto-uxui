8713bf5768b36734a90a87ada40f35044e7c28ce
Thauany Moedano <thauany@zappts.com>
2022-01-23 15:23:27 -0300

    Updates

---
+++ precache-manifest.d609e4fbb77b9c50f80ede7382cbdb68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4047b2fdcb0624835870b7912cdd132f",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "2e41918cb89dd2b114f7",
+    "url": "/react-app/static/css/main.9113b628.chunk.css"
+  },
+  {
+    "revision": "10ae0603604359519bfb",
+    "url": "/react-app/static/js/2.50c414a1.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-app/static/js/2.50c414a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e41918cb89dd2b114f7",
+    "url": "/react-app/static/js/main.e2bf0497.chunk.js"
+  },
+  {
+    "revision": "a0869fd3ed4d6a5b8e7c",
+    "url": "/react-app/static/js/runtime-main.d74d1e89.js"
+  }
+]);
